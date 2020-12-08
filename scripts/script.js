6491ef59be266add6ff1b524fea80a9487f33e61
lvangroningen <74056472+lvangroningen@users.noreply.github.com>
2020-12-08 17:06:09 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+function ul(index) {
+	console.log("click!" + index);
+
+	var underlines = document.querySelectorAll(".underline");
+
+	for (var i = 0; i < underlines.length; i++) {
+		underlines[i].style.transform = "translate3d(" + index * 100 + "%,0,0)";
+	}
+}

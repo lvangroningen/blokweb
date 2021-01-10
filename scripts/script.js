cdf408519df57eff593e5cf89e4b0dd7874ea44e
lvangroningen <74056472+lvangroningen@users.noreply.github.com>
2021-01-10 17:40:22 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,1 @@
 // JavaScript Document
-
-/* zwarte underline menu */
-function ul(index) {
-	console.log("click!" + index);
-
-	var underlines = document.querySelectorAll(".underline");
-
-	for (var i = 0; i < underlines.length; i++) {
-		underlines[i].style.transform = "translate3d(" + index * 100 + "%,0,0)";
-	}
-}
